cd156faa2d24637891d6a348e8e4ac2406566eca
asmith-dev <8sm1thar@tutanota.com>
2022-10-06 12:28:15 -0400

    Built the lexer, added tokens, made a function to check for floating-point parts, and updated/added comments

--- src/main.go
+++ src/main.go
@@ -1,28 +1,101 @@
 /*
-Calculator made in Go.
+	Calculator made in Go.
 
-Gets an expression from the user, evaluates it, and returns the result.
-Expression will be evaluated using the order of operations.
-Valid data type will be float64.
-Valid operations will be +, -, *, and /.
-Parentheses will be acceptable.
+	Gets an expression from the user, evaluates it, and returns the result.
+	Expression will be evaluated using the order of operations.
+	Supports the use of negatives and floats.
+	Valid operations will be +, -, *, and /.
+	Parentheses will be acceptable.
 */
 
 package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 )
 
-// Separates a given expression into tokens and categorizes each.
-// Need to implement categorizing and error handling for invalid symbols
-func lexer(str string) map[string]string {
-	lexed := make(map[string]string) // placeholder for the return statement
+// Tokens for the lexer:
 
-	// This for loop will iterate through the expression,
-	// evaluate each char, and append to the map
+var LP = "LPAREN"
+var RP = "RPAREN"
+var A = "ADD"
+var S = "SUB/NEG"
+var M = "MULTI"
+var D = "DIV"
+var N = "NUM:"
+
+// Checks if a char is part of a floating-point number, i.e. in {0,1,2,3,4,5,6,7,8,9,.}
+func isFloatPart(str string) bool {
+	_, err := strconv.ParseInt(str, 10, 8)
+
+	if err == nil || str == "." {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Separates a given expression into tokens
+// Error-handling for invalid symbols and invalid parenthesis count
+func lexer(str string) []string {
+	var lexed []string
+	var parenCount int8
+	var tempNum string
+
+	// This loop iterates through the expression,
+	// evaluates each char, and appends to the lexed slice.
 	for i := 0; i < len(str); i++ {
-		fmt.Print(string(str[i]))
+		tempChar := string(str[i])
+
+		// Checks if a number was being made, but is now ended
+		// If true, this appends a token for the number
+		if !isFloatPart(tempChar) && tempNum != "" {
+			lexed = append(lexed, N+tempNum)
+			tempNum = ""
+		}
+
+		// Tokenizes symbols
+		// Throws error for invalid characters
+		switch tempChar {
+		case "(":
+			lexed = append(lexed, LP)
+			parenCount++
+		case ")":
+			lexed = append(lexed, RP)
+			parenCount--
+		case "+":
+			lexed = append(lexed, A)
+		case "-":
+			lexed = append(lexed, S)
+		case "*":
+			lexed = append(lexed, M)
+		case "/":
+			lexed = append(lexed, D)
+		default:
+			if !isFloatPart(tempChar) {
+				log.Fatal("Invalid character entered: " + tempChar)
+			}
+		}
+
+		// Checks parenthesis count and throws errors for invalid values
+		if parenCount < 0 {
+			log.Fatal("Syntax error: \")\" before \"(\"")
+		} else if parenCount != 0 && i == len(str)-1 {
+			log.Fatal("Syntax error: unmatched \"(\"")
+		}
+
+		// Builds numbers char by char. Works for integers and floats
+		if isFloatPart(tempChar) {
+			tempNum += tempChar
+		}
+
+		// If the loop is ending and a number is being built,
+		// then appends the number
+		if i == len(str)-1 && tempNum != "" {
+			lexed = append(lexed, N+tempNum)
+		}
 	}
 
 	return lexed
@@ -31,5 +104,6 @@ func lexer(str string) map[string]string {
 func main() {
 	// Demonstrates usage of the input function.
 	getInput := input("Enter an equation (without spaces): ")
-	lexer(getInput)
+	lexedExpression := lexer(getInput)
+	fmt.Println(lexedExpression)
 }
