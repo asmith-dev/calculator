4ad16868e41a4961403f48e099bf3c3220143bc6
asmith-dev <8sm1thar@tutanota.com>
2022-10-20 13:04:07 -0400

    Updated notes and updated comments in the usefulpkg

--- pkg/usefulpkg.go
+++ pkg/usefulpkg.go
@@ -1,8 +1,5 @@
 /*
 	This script includes a variety of useful functions to condense workflow.
-	It only needs added to the same directory as the file using the functions.
-	Alternatively, individual functions can be copied and pasted into the file.
-	In that case, the "import" statement will need updated accordingly.
 */
 
 package pkg
